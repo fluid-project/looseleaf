b52ac39e12bac75e2d88044a1adf2e71e0e818bd
Ned Zimmerman <ned@bight.dev>
2021-07-27 09:46:49 -0300

    feat: initial table implementation

---
+++ src/components/layouts/table/table.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    order: 6,
+    status: "wip",
+    context: {
+        caption: "Members of the party.",
+        headers: {
+            "name": "Name",
+            "email": "E-mail",
+            "role": "Role"
+        },
+        records: [
+            {
+                "name": "Bilbo Baggins",
+                "email": "bilbo.baggins@hotmail.com",
+                "role": "Rogue"
+            },
+            {
+                "name": "Gandalf the Grey",
+                "email": "gandalf@icloud.com",
+                "role": "Wizard"
+            },
+            {
+                "name": "Thorin Oakenshield",
+                "email": "oakenshield@gmail.com",
+                "role": "Fighter"
+            }
+        ]
+    }
+};
