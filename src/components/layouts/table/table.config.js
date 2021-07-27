780dde401aa305b26e46934dbf22dad197258296
Ned Zimmerman <ned@bight.dev>
2021-07-27 12:33:47 -0300

    feat: add row headers

--- src/components/layouts/table/table.config.js
+++ src/components/layouts/table/table.config.js
@@ -4,36 +4,72 @@ module.exports = {
     context: {
         caption: "Members of the party.",
         captionSide: "top",
-        columnHeaders: {
-            "name": "Name",
-            "email": "E-mail",
-            "role": "Role"
-        },
-        rowHeaders: {},
+        columnHeaders: [
+            "Name",
+            "E-mail",
+            "Role"
+        ],
+        rowHeaders: [],
         records: [
-            {
-                "name": "Bilbo Baggins",
-                "email": "bilbo.baggins@hotmail.com",
-                "role": "Rogue"
-            },
-            {
-                "name": "Gandalf the Grey",
-                "email": "gandalf@icloud.com",
-                "role": "Wizard"
-            },
-            {
-                "name": "Thorin Oakenshield",
-                "email": "oakenshield@gmail.com",
-                "role": "Fighter"
-            }
+            [
+                "Bilbo Baggins",
+                "bilbo.baggins@hotmail.com",
+                "Rogue"
+            ],
+            [
+                "Gandalf the Grey",
+                "gandalf@icloud.com",
+                "Wizard"
+            ],
+            [
+                "Thorin Oakenshield",
+                "oakenshield@gmail.com",
+                "Fighter"
+            ]
         ]
     },
     variants: [
         {
             name: "bottom-caption",
-            label: "Bottom caption",
+            label: "Bottom Caption",
             context: {
                 captionSide: "bottom"
+            }
+        },
+        {
+            name: "row-headers",
+            label: "Row Headers",
+            context: {
+                caption: "First quarter revenue and expenses.",
+                captionSide: "top",
+                columnHeaders: [
+                    "",
+                    "Revenue",
+                    "Expenses",
+                    "Net Income"
+                ],
+                rowHeaders: [
+                    "January",
+                    "February",
+                    "March"
+                ],
+                records: [
+                    [
+                        "1800.00",
+                        "1375.00",
+                        "425.00"
+                    ],
+                    [
+                        "2100.00",
+                        "1675.00",
+                        "425.00"
+                    ],
+                    [
+                        "2250.00",
+                        "1375.00",
+                        "875.00"
+                    ]
+                ]
             }
         }
     ]
